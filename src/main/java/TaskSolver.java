439d1794e124edb878ecc105558756a490219228
Scumbrum <73284043+Scumbrum@users.noreply.github.com>
2021-03-20 14:38:09 +0200

    add tasks

--- src/main/java/TaskSolver.java
+++ src/main/java/TaskSolver.java
@@ -18,9 +18,6 @@ public class TaskSolver extends HttpServlet {
             Double b = Double.parseDouble(request.getParameter("b"));
             Double c = Double.parseDouble(request.getParameter("c"));
             Double d = Double.parseDouble(request.getParameter("d"));
-        } catch (NumberFormatException e){
-            response.sendError(400);
-        }
 
         try(PrintWriter out = response.getWriter()){
             out.println("<!DOCTYPE html>");
@@ -29,18 +26,24 @@ public class TaskSolver extends HttpServlet {
             out.println(" <body>");
             switch (request.getParameter("taskNumber")){
                 case "7":
-                    out.println("дарова1");
+                    double result=Math.pow(4*Math.cosh(Math.sqrt(a/b))+3*Math.acos(c),d);
+                    out.println(result);
                     break;
                 case "8":
-                    out.println("дарова2");
+                    double result1=5*a/Math.sin(a)+Math.sqrt(Math.tanh(Math.abs(b)*c)/Math.log(d));
+                    out.println(result1);
                     break;
                 case "9":
-                    out.println("дарова3");
+                    double result2=(Math.sin(Math.abs(a))+Math.cos(Math.sqrt(b)))/(2*Math.tan(c)+Math.pow(Math.E,d));
+                    out.println(result2);
                     break;
             }
             out.println(" </body>");
             out.println("</html>");
         }
+    } catch (NumberFormatException e){
+        response.sendError(400);
+    }
 
 
     }
